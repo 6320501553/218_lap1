b2f87f184e6410d19e301b1ef55c2c970e402708
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-04 15:53:10 +0700

    add

--- 6320501553_1_3.c
+++ 6320501553_1_3.c
@@ -2,8 +2,8 @@
 #include<math.h>
 int main()
 {
-    unsigned int a;
-    scanf("%lu",&a);
-    printf("%lu %lu",a%3,a%11);
+    int a;
+    scanf("%d",&a);
+    printf("%d %d",a%3,a%11);
     return 0;
 }
