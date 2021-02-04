634f017c1de16fde541bdeb56d3e55e6f9b46573
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-04 17:30:36 +0700

    add

--- 6320501553_1_7.c
+++ 6320501553_1_7.c
@@ -3,9 +3,9 @@ int main()
 {
     float time;
     scanf("%f",&time);
-    if(n>=12.00&&n<=23.59)
+    if(time>=12.00&&time<=23.59)
         printf("%.2f p.m.",time);
-    else if(time<=11.59&&time>=00.00)
+    else if(time<12.00&&time>=00.00)
         printf("%.2f a.m.",time);
     return 0;
 }
