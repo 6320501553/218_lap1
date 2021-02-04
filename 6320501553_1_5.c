84ee8b9dcd76f14adddebe0e0a8db40387ad121a
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-04 16:03:35 +0700

    add

---
+++ 6320501553_1_5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,max=0;
+    for(i=1; i<=n; i++)
+        scanf("%d",&a[i]);
+    for(i=1; i<=n; i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+}
