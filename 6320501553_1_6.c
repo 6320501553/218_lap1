df97ccc893ec1243fac2c6aaed1cf064f8cfd2f9
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-11 14:49:10 +0700

    add

--- 6320501553_1_6.c
+++ 6320501553_1_6.c
@@ -2,8 +2,6 @@
 int main()
 {
     int n,m,t;
-    char k;
     scanf("%d %d %d",n,m,t);
-    fflush(stdin);
-    scanf("%c",&k);
+
 }
--- 6320501553_1_7.c
+++ 6320501553_1_7.c
@@ -2,9 +2,13 @@
 int main()
 {
     float time;
+    int n;
     scanf("%f",&time);
+    n=(int)time;
+    m=(int)(time*100)%100;
+
     if(time>=12.00&&time<=23.59)
-        printf("%.2f p.m.",time);
+        printf("%d: p.m.",n,time);
     else if(time<12.00&&time>=00.00)
         printf("%.2f a.m.",time);
     return 0;
